06c51bde0179d3cdf6813ac2e1723eaa45be85f3
Anna Russi <annarussi@gmail.com>
2023-07-07 14:13:18 +0100

    created form and list of taks

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
 import "./styles.css";
 
 export default function App() {
-  return "Hi!"
+  return(
+    <>
+      <form>
+        <div className="mb-3">
+          <label htmlFor="item" className="form-label">New Task</label>
+          <input type="text" id="item" className="form-control" />
+        </div>
+        <button className="btn btn-primary">Add</button>
+      </form>
+
+      <h1 className="text-center mb-3">Tasks</h1>
+
+      <ul className="list-group">
+        <li className="list-group-item">
+          <label>
+            <input type="checkbox"/>
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+
+      </ul>
+    </>
+  )
 }
