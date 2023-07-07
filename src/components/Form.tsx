73de942d72899e50879f5de45fd56fd3aa1dcd63
Anna Russi <annarussi@gmail.com>
2023-07-07 16:44:32 +0100

    created teh Tasks and TaskItem component

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -8,7 +8,7 @@ export default function Form({ onSubmit }: FormProps) {
 
   const [newTask, setNewTask] = useState <string>("");
 
-  function handleSumit(e: React.FormEvent<HTMLFormElement>) {
+  function handleSumit(e: { preventDefault: () => void; }) {
     e.preventDefault()
     if (newTask === "") return
 
@@ -22,7 +22,7 @@ export default function Form({ onSubmit }: FormProps) {
     <form onSubmit={handleSumit}>
         <div className="mb-3">
           <label htmlFor="item" className="form-label">New Task</label>
-          <input value={newTask} onChange={e => setNewTask(e.target.value)} type="text" id="item" className="form-control" />
+          <input value={newTask} onChange={e => setNewTask(e.target.value)} type="text" id="item" name="item" className="form-control" />
         </div>
         <button className="btn btn-primary">Add</button>
     </form>
